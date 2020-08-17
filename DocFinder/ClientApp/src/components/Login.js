0b53a23fdcc2824fbd0dc18ebfa02be1d94b4026
sriram26 <sriram.sakinala@gmail.com>
2020-08-17 00:51:54 -0500

    Added Login file for testing

---
+++ DocFinder/ClientApp/src/components/Login.js
@@ -0,0 +1,22 @@
+﻿import React, { Component } from 'react';
+
+
+export function LoginComponent() {
+
+    return (
+        <div>
+            <h1>Login</h1>
+
+            <p>This is a simple example of a Login component.</p>
+
+            <p aria-live="polite">Current count:</p>
+
+            <button className="btn btn-primary"> Increment</button>
+        </div>
+
+    );
+}
+
+
+
+
