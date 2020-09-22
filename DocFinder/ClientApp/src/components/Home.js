a2eea420cbc70c482339cd5cf2f65b57f4846d3a
Praneeth <praneeth.gurujala@outlook.com>
2020-09-21 23:47:04 -0500

    Added cariusel images

--- DocFinder/ClientApp/src/components/Home.js
+++ DocFinder/ClientApp/src/components/Home.js
@@ -1,5 +1,8 @@
 import React, { Component, useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import Banner1 from '../images/banner1.jpg';
+import Banner2 from '../images/banner2.jpg';
+import Banner3 from '../images/banner3.jpg';
 
 
 export function Home() {
@@ -14,37 +17,37 @@ export function Home() {
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="images/banner1.jpg"
+                    src={Banner1}
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    <h3>How to list yourself</h3>
+                    <p>List yourself with us</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="../../images/banner2.jpg"
+                    src={Banner2}
                     alt="Second slide"
                 />
 
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <h3>How to list your hospital</h3>
+                    <p>List your hospital with us</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="././images/banner3.jpg"
+                    src={Banner3}
                     alt="Third slide"
                 />
 
                 <Carousel.Caption>
-                    <h3>Third slide label</h3>
+                    <h3>Health care Professional</h3>
                     <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                       Be a part of our family
           </p>
                 </Carousel.Caption>
             </Carousel.Item>
